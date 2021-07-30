68a4d34f2c777f632bccada8d7df39b08d34744d
riyapurohit <purohitriya455@outlook.com>
2021-07-30 11:40:04 +0530

    a

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,6 @@ test('renders learn react link', () => {
   render(<App />);
   
   const linkElement = screen.getByText("Learn React");
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument(a);
 
 });
